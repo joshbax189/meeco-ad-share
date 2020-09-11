d6eb8f4277578f258be76ce79ff122480682e778
Josh Bax <joshbax189@gmail.com>
2020-09-11 21:00:01 +1200

    Create Invitation using a key

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import * as Meeco from '@meeco/sdk';
 import { Keypair, KeypairResponse } from '@meeco/keystore-api-sdk';
+import { Connection } from '@meeco/vault-api-sdk';
 import * as cryppo from '@meeco/cryppo';
 import * as m from 'mithril';
-
-// import "./styles.scss";
 
 import { TemplateSchemaStore, ItemTemplate } from './TemplateSchemaStore';
 // import JSONComponent from './JSONComponent.js';
@@ -221,85 +220,158 @@ async function connectHandler(invitationToken: string): Promise<Connection> {
     .then(res => res.connection);
 }
 
-// function getInvite() {
-//   // this is done serverside before drawing the thing
-//   const keyPair = await createAndStoreKeyPair(auth);
-//   // should just use service key
+async function createKeyPair(keyId: string, keystore_token: string) {
+    console.log('creating a key for connection');
 
-//   return await this.vaultApiFactory(authToken)
-//       .InvitationApi.invitationsPost({
-//         public_key: {
-//           keypair_external_id: keyPair.keystoreStoredKeyPair.id,
-//           public_key: keyPair.keyPair.publicKey,
-//         },
-//         invitation: {
-//           encrypted_recipient_name: 'Bob'
-//         },
-//       })
-//       .then(result => result.invitation);
-// }
+    const keyPairUn = await cryppo.generateRSAKeyPair();
 
-function showForm() {
-  // document.getElementById('auth').show();
-  // form on submit creates Meeco share
-  // notifies advertiser
+    return cryppo.encryptWithKey({
+      data: keyPairUn.privateKey,
+      key: AuthData.key_encryption_key.key,
+      strategy: cryppo.CipherStrategy.AES_GCM,
+    }).then(privateKeyEncrypted =>
+      m.request({
+        method: 'POST',
+        url: environment.keystore.url + '/keypairs',
+        headers: makeAuthHeaders(keystore_token),
+        body: {
+        public_key: keyPairUn.publicKey,
+        encrypted_serialized_key: privateKeyEncrypted.serialized,
+        // API will 500 without
+        metadata: {},
+          external_identifiers: [keyId],
+        }
+      }))
+      .then((result: KeypairResponse) => {
+        console.log(result.keypair);
+        return result.keypair;
+      });
 }
 
+async function createInvite(vaultToken: string, keystoreToken: string, keyPairId: string, encryptedName: string) {
+  // for other-user:
+  const keyPair = await createKeyPair(keyPairId, keystoreToken);
+
+  console.log('creating invite');
+  return Meeco.vaultAPIFactory(environment)(vaultToken)
+      .InvitationApi.invitationsPost({
+        public_key: {
+          keypair_external_id: keyPairId,
+          public_key: keyPair.public_key,
+        },
+        invitation: {
+          encrypted_recipient_name: encryptedName,
+        },
+      })
+    .then(result => {
+      console.log(result.invitation);
+      return result.invitation;
+    });
+}
+
+async function createInviteFromKey(vaultToken: string, publicKey: string, keyPairId: string, encryptedName: string) {
+  console.log('creating invite');
+  return m.request({
+    method: 'POST',
+    url: environment.vault.url + '/invitations',
+    headers: makeAuthHeaders(vaultToken),
+    body: {
+        public_key: {
+          // keypair_external_id: keyPairId,
+          keypair_id: '8a3abe43-2c35-4ad4-9075-80a1aef763ba',
+          encryption_strategy: 'Aes256Gcm',
+          public_key: publicKey,
+        },
+        invitation: {
+          encrypted_recipient_name: encryptedName,
+          message: 'hi mom',
+          email: 'joshbax189@gmail.com'
+        },
+    }
+  }).then((result: any) => {
+      console.log(result.invitation);
+      return result.invitation;
+    });
+}
+
+
 window.onload = () => {
+  document.getElementById('test-form').hidden = true;
+
   APIs.init();
 
   m.mount(document.getElementById('auth'), LoginComponent);
+
+  // createInvite(ServiceUserAuth.vault_access_token, ServiceUserAuth.keystore_access_token, 'funny_hat', 'fake-name');
+//   createInviteFromKey(ServiceUserAuth.vault_access_token, '-----BEGIN PUBLIC KEY-----\
+// MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA1eprH/3l2nwWGRASsdMP\
+// V7TLOKR0+F1aKcTW3IUXKsdDUqYnTxdCdgKCfl4UCUzQot3IuxwNx6fQAa/SWlB+\
+// tNlJXvWZcJ3bkXuys5HAnMut6z5d3sa08lFS5FXCV1KajHCRZEhe5mn77MknHl1+\
+// hx7zsjXQ4b7YlzWGmPUdotTDG3HYgceAr4hOClDxgBmkMAIXGFGq9qXR1J4dHlJQ\
+// 5u7poge+kWazM7jjctdkiuC4eH87znb8jijl8lHpqsDyT/F7oMChWonNgDHKHgnY\
+// Mrn1A15h6I80hqzHgdwPuiV+pbi2E3ggfVR7TF2W4gR+6GCbBuvUg2O0Z0KInjfJ\
+// +AOIz2zfIezKBh3WzS45GBy8LK1BFvqEkxVzivd5AaPlTHfu7PlGVPiRERpSkA5H\
+// vvyVxK6jjt50vjtiaOUfwVhX/CxFn5u5g+LZJk6nCAxl6zSx7bLAnSIocTAhoqLr\
+// aJ2Xju0qNpyutjFUtIkWnbpWgpXHOzTeVuPPTZVCkaEkuCPfzyBVaiLMvyS/bMd0\
+// bpUbYJxuNsRrcZ/O1pzMDivFBGq8vu+X+xCG/d3TSk8a+2hiejb9Km+TMVZGiZIF\
+// +dzTnxp5mn2GjfXxV57oU2cNjheM9qklEPUpmv+dxfX2kj5RsqPCadzs+Th/4ndl\
+// Czfb6M8JtF3lQjpjp6cfNk8CAwEAAQ==\
+// -----END PUBLIC KEY-----', 'funny_hat', 'Aes256Gcm.6xtPqA==.LS0tCml2OiAhYmluYXJ5IHwtCiAgWG9mS2U1WTBodmJPbVlrRAphdDogIWJpbmFyeSB8LQogIGErMi95SXZ2dnBMQytmeVdmYjVWekE9PQphZDogbm9uZQo=');
 
   document.getElementById('ad-target').onclick = () => {
     alert('I send connect request!');
     // Get Invite
     const inviteToken = document.getElementById('ad-target').attributes.getNamedItem('data-meeco-invite').value;
 
-    let connection = connectHandler(inviteToken).then(c => {
-    // get back recipient_id
-      console.log(c);
-      return c;
-    });
+    // let connection = connectHandler(inviteToken).then(c => {
+    //   // get back recipient_id
+    //   console.log('connection is');
+    //   console.log(c);
+    //   return c;
+    // });
+
     // let recipient = connection.user_id;
     // also some form_id for 'next_steps'
+    // then show form
+    document.getElementById('test-form').hidden = false;
+
+    const templateName = 'fake_template';
+
+    APIs.templates.loadTemplates().then(() => {
+      console.log('creating template');
+      makeFormTemplate('test-form');
+
+      let template = APIs.templates.getTemplateByName(templateName);
+
+      // Items may exist!
+      lookupItem(template.id).then(existingItems => {
+
+        if (existingItems.length > 0) {
+          console.log('autofill');
+          document.getElementById('test-form').insertAdjacentHTML('afterend', '<button>Autofill</button>');
+        }
+
+        document.getElementById('submit-target').onclick = e => {
+          e.preventDefault();
+          // TODO Create Connect
+          // const service = Meeco.ConnectionService(environment).createConnection({from: _, to: _, options: _});
+
+          createItem(templateName, collectSlotData()).then(d => {
+            console.log(d);
+            m.mount(document.getElementById('item-output'), JSONComponent(d));
+          });
+          // Once created -> share with Org/Service
+
+          // TODO
+          // Get {OrgId/service/Id}.agent_id
+
+          // TODO Create Connect
+
+          // TODO Create Share
+          // TODO Send Invite
+        };
+
+      });
+    });
   };
-
-  let fieldNames = [];
-  document.querySelectorAll('#test-form input').forEach((x: any) => fieldNames.push(x.name));
-
-  // APIs.templates.loadTemplates().then(() => {
-  //   console.log('creating template');
-  //   makeTemplate(templateName, fieldNames);
-
-  //   let template = APIs.templates.getTemplateByName(templateName);
-
-  //   lookupItem(template.id).then(existingItems => {
-
-  //     if (existingItems.length > 0) {
-  //       console.log('autofill');
-  //       document.getElementById('test-form').insertAdjacentHTML('afterEnd', '<button>Autofill</button>');
-  //     }
-
-  //     document.getElementById('submit-target').onclick = e => {
-  //       e.preventDefault();
-  //       // TODO Create Connect
-  //       Meeco.ConnectionService(environment).createConnection({from: _, to: _, options: _});
-
-  //       createItem(templateName, collectSlotData()).then(d => {
-  //         console.log(d);
-  //         m.mount(document.getElementById('item-output'), JSONComponent(d));
-  //       });
-  //       // Once created -> share with Org/Service
-
-  //       // TODO
-  //       // Get {OrgId/service/Id}.agent_id
-
-  //       // TODO Create Connect
-
-  //       // TODO Create Share
-  //       // TODO Send Invite
-  //     };
-
-  //   });
-  // });
 }
